9aa8de976e819dffb4df49c5de093a3e20ad4495
Thomas Haller <thomashaller@gmx.at>
2021-05-09 21:58:10 +0200

    deploy scripts: ensuring node directory on target machine

--- testnet/control/build-and-deploy-nodes.sh
+++ testnet/control/build-and-deploy-nodes.sh
@@ -4,6 +4,11 @@
 
 
 #
+ssh hbbft1 'mkdir -p ~/node/node/'
+ssh hbbft2 'mkdir -p ~/node/node/'
+ssh hbbft3 'mkdir -p ~/node/node/'
+ssh hbbft4 'mkdir -p ~/node/node/'
+
 scp -r ../nodes/node1/* hbbft1:~/node/node/
 scp -r ../nodes/node2/* hbbft2:~/node/node/
 scp -r ../nodes/node3/* hbbft3:~/node/node/

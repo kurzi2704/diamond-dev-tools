4a6b1b4a8e5a6077db3b6d6c9a561895b0c809a4
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-05 04:22:38 +0200

    control scripts extended to 7 nodes.

--- testnet/control/build-and-deploy-oe-hybrid.sh
+++ testnet/control/build-and-deploy-oe-hybrid.sh
@@ -9,12 +9,23 @@
 
 cd ../../../openethereum/
 
-cargo build
-scp target/debug/openethereum  hbbft1:~/node/node/
+cargo build --release
+scp target/release/openethereum  hbbft1:~/node/node/
 
 echo "copy openethereum to hbbft2..."
 ssh hbbft1 'scp ~/node/node/openethereum hbbft2:~/node/node/'
+
 echo "copy openethereum to hbbft3..."
 ssh hbbft1 'scp ~/node/node/openethereum hbbft3:~/node/node/'
+
 echo "copy openethereum to hbbft4..."
 ssh hbbft1 'scp ~/node/node/openethereum hbbft4:~/node/node/'
+
+echo "copy openethereum to hbbft5..."
+ssh hbbft1 'scp ~/node/node/openethereum hbbft5:~/node/node/'
+
+echo "copy openethereum to hbbft6..."
+ssh hbbft1 'scp ~/node/node/openethereum hbbft6:~/node/node/'
+
+echo "copy openethereum to hbbft7..."
+ssh hbbft1 'scp ~/node/node/openethereum hbbft7:~/node/node/'

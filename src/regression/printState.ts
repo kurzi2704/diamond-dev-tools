162600e316e1b32c7328563f86b027145df98f8d
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-09 22:41:37 +0200

    extented printState script by epoch start and finish time.

--- src/regression/printState.ts
+++ src/regression/printState.ts
@@ -5,15 +5,25 @@ import { ContractManager } from "../contractManager";
 import { NodeManager } from "./nodeManager";
 
 export async function printState(nodeManager: NodeManager, contractManager: ContractManager) {
+  
+
+  const validatorSet = contractManager.getValidatorSetHbbft();
+  const staking =  await contractManager.getStakingHbbft();
+
+  const epochStartTime = new Date(Number.parseInt(await staking.methods.stakingEpochStartTime().call()) * 1000);
+  const phaseTransition = new Date(Number.parseInt(await staking.methods.startTimeOfNextPhaseTransition().call()) * 1000);
+
+  console.log(`epoch start time UTC: ${epochStartTime.toUTCString()}`);
+  console.log(`next Phase Transition UTC: ${phaseTransition.toUTCString()}`);
 
   nodeManager.nodeStates.forEach(async (s) => {
     if (s.address) {
-      const validatorSet = contractManager.getValidatorSetHbbft();
+      
       const stakingAddress =  await validatorSet.methods.stakingByMiningAddress(s.address).call();
       const encodedABI = validatorSet.methods.validatorAvailableSince(stakingAddress).encodeABI();
       // console.log(`call Encoded for ${s.address}:`, encodedABI);
       const callResult = await validatorSet.methods.validatorAvailableSince(stakingAddress).call();
-      console.log(`call result for ${s.address}`, callResult);
+      console.log(`${s.address} available since: `, callResult);
       //s.address
     }
   

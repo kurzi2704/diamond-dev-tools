694c0850223eae377b0752ee90f12891f83b7b65
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-09 22:46:55 +0200

    formating available date in printState.

--- src/regression/printState.ts
+++ src/regression/printState.ts
@@ -18,15 +18,13 @@ export async function printState(nodeManager: NodeManager, contractManager: Cont
 
   nodeManager.nodeStates.forEach(async (s) => {
     if (s.address) {
-      
       // const stakingAddress =  await validatorSet.methods.stakingByMiningAddress(s.address).call();
       // const encodedABI = validatorSet.methods.validatorAvailableSince(stakingAddress).encodeABI();
       // console.log(`call Encoded for ${s.address}:`, encodedABI);
       const callResult = await validatorSet.methods.validatorAvailableSince(s.address).call();
-      console.log(`${s.address} available since: `, callResult);
+      console.log(`${s.address} available since: ${callResult} ${new Date(Number.parseInt(callResult) * 1000).toUTCString()}`);
       //s.address
     }
-  
   })
 
 }

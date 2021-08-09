f3bf9faede982e4bc03fca37b661d082f357a20a
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-09 22:59:15 +0200

    picked up cmd function to become reusable.

--- src/remoteCommand.ts
+++ src/remoteCommand.ts
@@ -3,12 +3,22 @@ import * as child from 'child_process';
 //executes a command on a remote Node.
 export function cmdR(hostSSH: string, command: string) : string {
   
-  console.log(command);
+  //console.log(command);
   //todo: proper escaping for the shell of command here.
   const remoteCommand = `ssh -t -o LogLevel=QUIET ${hostSSH} '${command}'`;
+  //console.log(remoteCommand);
   console.log(`executing on ${hostSSH}: ${remoteCommand}`);
   const result = child.execSync(remoteCommand);
   const txt = result.toString();
   console.log(txt);
   return txt;
 }
+
+export function cmd(command: string) : string {
+  console.log(command);
+  const result = child.execSync(command);
+  const txt = result.toString();
+  console.log(txt);
+  return txt;
+}
+

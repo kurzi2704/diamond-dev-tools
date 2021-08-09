ef37b4b1354a4ffc95d1588ed720bceb6089d691
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-09 22:56:51 +0200

    delete message script

---
+++ src/remotenet/runDeleteMessages.ts
@@ -0,0 +1,6 @@
+import { executeOnAllRemotes } from "./executeOnAllRemotes";
+
+
+
+//todo find better command, this kind of hard kills it.
+executeOnAllRemotes("rm -r ~/hbbft_testnet/node/data/messages");

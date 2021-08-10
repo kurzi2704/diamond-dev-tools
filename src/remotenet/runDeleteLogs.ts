8cce81b4337ad9065cdc04bf8ce40d4ae8dc5b51
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-10 15:36:08 +0200

    script for deleting logs and chaindata

---
+++ src/remotenet/runDeleteLogs.ts
@@ -0,0 +1,6 @@
+import { executeOnAllRemotes } from "./executeOnAllRemotes";
+
+
+
+//todo find better command, this kind of hard kills it.
+executeOnAllRemotes("rm ~/hbbft_testnet/node/parity.log");

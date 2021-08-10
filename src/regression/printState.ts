147c9c79e43f7c86258d01395e014c2fb878fde3
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-10 18:02:40 +0200

    print state: key gen history information

--- src/regression/printState.ts
+++ src/regression/printState.ts
@@ -9,6 +9,7 @@ export async function printState(nodeManager: NodeManager, contractManager: Cont
 
   const validatorSet = contractManager.getValidatorSetHbbft();
   const staking =  await contractManager.getStakingHbbft();
+  const keyGenHistory = await contractManager.getKeyGenHistory();
 
   const epochStartTime = new Date(Number.parseInt(await staking.methods.stakingEpochStartTime().call()) * 1000);
   const phaseTransition = new Date(Number.parseInt(await staking.methods.startTimeOfNextPhaseTransition().call()) * 1000);
@@ -20,7 +21,22 @@ export async function printState(nodeManager: NodeManager, contractManager: Cont
   console.log(`Epoch End Time: UTC: ${epochEndTime.toUTCString()}`);
 
 
-  console.log(`pending validators:`, await validatorSet.methods.getPendingValidators().call());
+  const pendingValidators = await validatorSet.methods.getPendingValidators().call()
+  console.log(`pending validators:`, pendingValidators);
+
+  for (let i = 0; i < pendingValidators.length; i++) {
+    const pendingValidator = pendingValidators[i];
+    const currentKeyGenMode = await validatorSet.methods.getPendingValidatorKeyGenerationMode(pendingValidator).call();
+    console.log(`pending validator ${pendingValidator} key gen mode: `, currentKeyGenMode);
+  }
+
+  if (pendingValidators.length > 0) {
+    
+    const numberOfKeyFragmentsWritten = await keyGenHistory.methods.getNumberOfKeyFragmentsWritten().call();
+    console.log(`number of key fragments written:`, numberOfKeyFragmentsWritten);
+  }
+
+  //validatorSetContract.getPendingValidatorKeyGenerationMode(_sender)
 
   console.log(`likehilihood:`, await staking.methods.getPoolsLikelihood().call());
 

1e4c56df51d33d5397b584513c3044d3e7e4e9f3
Thomas Haller <Thomas.Haller@lab10.coop>
2021-10-21 20:01:50 +0200

    deploy binaries now checks if the file really needs to get transfered.

--- src/remotenet/runDeployBinaries.ts
+++ src/remotenet/runDeployBinaries.ts
@@ -5,6 +5,9 @@ import { cmd, cmdR } from '../remoteCommand';
 import { getNodesFromCliArgs } from './remotenetArgs';
 
 
+function getSha1FromCmdResult(cmdResult: string) : string {
+  return cmdResult.substring(0, 40);
+}
 
 async function run() {
 
@@ -14,11 +17,27 @@ async function run() {
 
   console.log('Looking up local nodes directory:', nodesDirAbsolute);
 
+  const localBinary = `../openethereum/target/release/openethereum`;
+  const sha1LocalCmdResult = cmd(`sha1sum ${localBinary}`);
+  const sha1Local = getSha1FromCmdResult(sha1LocalCmdResult);
+
+  console.log(`updating to openethereum client wtih sha1sum: ${sha1Local}`);
+
   for(let i = 0; i < nodes.length; i++) {
     const node = nodes[i];
     console.log(`=== Node ${node.nodeID} ===`);
 
     const nodeName = `hbbft${node.nodeID}`;
+    //todo: handling for first time install. probably this will crash if there is no openethereum available on target.
+    const sha1RemoteCmdResult = cmdR(nodeName, `sha1sum ~/hbbft_testnet/node/openethereum`);
+    const sha1Remote = getSha1FromCmdResult(sha1RemoteCmdResult);
+    console.log(`sha1remote: ${sha1Remote}`);
+
+    if (sha1Local == sha1Remote) {
+      console.log(`${nodeName} already up to date, skipping binary update.`);
+      continue;
+    }
+
     console.log('deploying openethereum executable.');
     const scpCommandExe = `scp ../openethereum/target/release/openethereum ${nodeName}:~/hbbft_testnet/node`;
     cmd(scpCommandExe);

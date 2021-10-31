a83e3bd3e0f00dc9bd1ccc0094b9b690225d7b3e
Thomas Haller <Thomas.Haller@lab10.coop>
2021-10-31 23:55:30 +0100

    download logs script

---
+++ src/remotenet/runDownloadLogs.ts
@@ -0,0 +1,84 @@
+import { NodeState } from "../regression/nodeManager";
+import { cmd, cmdR } from "../remoteCommand";
+import { getNodesFromCliArgs } from "./remotenetArgs";
+import fs from 'fs';
+import { nowFormatted } from "../utils/dateUtils";
+
+async function run() {
+
+  const date = nowFormatted();
+
+  const outputFileRemote = 'log_4_download.log';
+  const outputDirectory = `testnet/testnet-analysis/logs/${date}/`;
+  const remoteDirectory = '~/hbbft_testnet/node/';
+
+  console.log(`creating filtered log file`);
+  console.log('ensuring output directory: ', outputDirectory);
+
+  cmd('mkdir -p ' + outputDirectory);
+
+  // const cmd_boring_connections = 'INFO parity_ws::io  Accepted a new tcp connection from';
+  
+  
+
+  const command  = `cat ${remoteDirectory}parity.log | sed '/Rejecting recently rejected/d' | sed '/Rejected tx already in the blockchain/d' | sed '/Accepted a new tcp connection from/d' > ${remoteDirectory}${outputFileRemote}`;
+  const nodes = await getNodesFromCliArgs();
+  
+
+
+  async function workNodeAsync(node: NodeState) {
+
+    const sshName = node.sshNodeName();
+    const remoteFileFullPath = remoteDirectory + outputFileRemote;
+    const targetFileFullPath = `${outputDirectory}${sshName}.log`;
+    
+    if (fs.existsSync(targetFileFullPath)) {
+      console.log(`target file already found. skipping node ${sshName} - delete file if you want to execute operation again.`);
+      return;
+    }
+
+
+    try {
+      cmdR(sshName, `rm ${remoteFileFullPath}`);
+    } catch {
+      //just ignore file not found.
+      console.log(`could not remove old file to download`);
+    }
+
+    console.log(`${sshName} creating on ${outputFileRemote}`);
+    console.log(`${sshName} deleting current file if available:  ${outputFileRemote}`);
+
+
+    console.log(`${sshName} creating log file....`);
+    cmdR(sshName, command);
+    console.log(`${sshName} downloading created file log slice.`);
+    
+    cmd(`scp ${sshName}:${remoteFileFullPath} ${outputDirectory}${sshName}.log`);
+
+
+
+  }
+
+  //async.each(nodes, workNodeAsync);
+
+
+  const promisses = new Array<Promise<any>>();
+
+  console.log(`working all ${nodes.length} in parallel.`);
+
+  nodes.forEach(async (node) => {
+    const promise = new Promise(() => workNodeAsync(node));
+    promisses.push(promise);
+  });
+
+  console.log(`awaiting work.`);
+  for(const promise of promisses) {
+    await promise;
+  }
+  console.log(`work finished.`);
+
+  
+  
+}
+
+run();

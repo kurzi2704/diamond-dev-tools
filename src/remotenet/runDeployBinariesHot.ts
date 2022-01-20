5561a70a3f88e3b782ff9a0ea5ba3baf5f6cd984
Thomas Haller <Thomas.Haller@lab10.coop>
2022-01-20 17:57:32 +0100

    now sleeping on hot binary updates

--- src/remotenet/runDeployBinariesHot.ts
+++ src/remotenet/runDeployBinariesHot.ts
@@ -1,4 +1,5 @@
 import * as child from 'child_process';
+import { removeAdditionalFormatting } from 'ts-command-line-args';
 import { cmd, cmdR } from '../remoteCommand';
 import { executeOnRemotes, transferFilesToRemote, transferFilesToRemotes, transferFileToRemote } from './executeOnRemotes';
 import { getNodesFromCliArgs, parseRemotenetArgs } from './remotenetArgs';
@@ -6,6 +7,28 @@ import { getNodesFromCliArgs, parseRemotenetArgs } from './remotenetArgs';
 function getSha1FromCmdResult(cmdResult: string) : string {
   return cmdResult.substring(0, 40);
 }
+
+
+async function sleep(milliseconds: number) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+
+// TODO: somehow this command always results in an error when run 
+
+// function isScreenRunning(nodeName: string) {
+
+//   const command = 'screen -ls | grep node_test'
+//   const result = cmdR(nodeName, command);
+
+//   if (result.length === 0) {
+//     return false;
+//   }
+
+//   console.log('screen node_test is running on ', nodeName);
+//   console.log(result);
+//   return true;
+// }
 
 async function run() {
 
@@ -21,9 +44,7 @@ async function run() {
 
   for(const node of nodes) {
     
-    
     const nodeName = `hbbft${node.nodeID}`;
-
     const sha1RemoteCmdResult = cmdR(nodeName, `sha1sum ~/hbbft_testnet/node/openethereum`);
     const sha1Remote = getSha1FromCmdResult(sha1RemoteCmdResult);
 
@@ -32,18 +53,30 @@ async function run() {
       continue;
     }
 
-    console.log(`stopping ${nodeName}`);
-    try {
-      await cmdR(nodeName, 'screen -X -S node_test quit');
-    } catch (e) {
-      console.log('error durring stopping. probably screen not running. ignoring problem.');
-    }
+    //if (isScreenRunning(nodeName)) {
+      console.log(`stopping ${nodeName}`);
+      try {
+        cmdR(nodeName, 'screen -X -S node_test quit');
+      } catch (e) {
+        console.log('error durring stopping. probably screen not running. ignoring problem.');
+      }
+    //}
+
+    await sleep(2000);
+    
+    // if (isScreenRunning(nodeName)) {
+    //   console.log(`wait until screen for ${nodeName} stopps.`)
+    //   do {
+    //     await sleep(333);
+    //     process.stdout.write(".");
+    //   } while(isScreenRunning(nodeName))
+    // }
     
     console.log(`updating openethereum on ${nodeName}`);
     await transferFileToRemote(localBinary,nodeName);
     console.log(`starting node: ${nodeName}`);
     
-    await cmdR(nodeName, "screen -S node_test -d -m ~/hbbft_testnet/node/start.sh");
+    cmdR(nodeName, "screen -S node_test -d -m ~/hbbft_testnet/node/start.sh");
   };
   //todo find better command, this kind of hard kills it.
 

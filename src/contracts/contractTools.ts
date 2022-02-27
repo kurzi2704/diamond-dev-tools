69e62dd8114ab02929fc79850e7226967aa1085f
Thomas Haller <Thomas.Haller@lab10.coop>
2022-02-27 23:51:31 +0100

    moved artifactRequire

---
+++ src/contracts/contractTools.ts
@@ -0,0 +1,35 @@
+import Web3 from "web3";
+import fs from 'fs';
+
+export function artifactRequire(contractName: string) : any {
+
+  const filename = `./src/abi/json/${contractName}.json`;
+  if (!fs.existsSync(filename)){
+    throw Error(`contract not found: ${filename}`);
+  }
+  
+  const file = fs.readFileSync(filename, 'utf8');
+  return JSON.parse(file);
+
+}
+
+
+export async function deploy(web3: Web3, contractArtifact: any) : Promise<string> {
+
+  console.log('deploying contract...', contractArtifact.contractName );
+  //deployedContract
+  //const contract = new web3.eth.Contract();
+  var bc = contractArtifact.bytecode;
+  //var abi = contractArtifact.interface;
+  
+  const tx = await web3.eth.sendTransaction({gas: '10000000', data: bc});
+
+  if (!tx.contractAddress) {
+    throw Error('Expected new Contract address.');
+  }
+
+  //var contact = web3.eth.Contract new(abi,{from: web3.eth.accounts[0], data: bc});
+  
+  //simulate for now.
+  return tx.contractAddress;
+}

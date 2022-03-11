52dedbd92506a78e40ea65f8145a707a6e1e2811
Thomas Haller <Thomas.Haller@lab10.coop>
2022-03-11 21:40:44 +0100

    log headers now goes from back to front again.

--- src/runLogHeaders.ts
+++ src/runLogHeaders.ts
@@ -18,11 +18,11 @@ async function logHeaders() {
 
   for(let i = numOfBlocksToDisplay; i >= 0; i--) {
     
-    const blockToAnalyse = latestBlock - i;
+    const blockToAnalyse = i;
     const blockHeader = await web3.eth.getBlock(blockToAnalyse);
 
     const thisTimeStamp = Number.parseInt(String(blockHeader.timestamp));
-    console.log( `${blockHeader.number} ${blockHeader.hash} ${blockHeader.extraData} ${blockHeader.timestamp} ${new Date(thisTimeStamp * 1000).toUTCString()} ${thisTimeStamp - lastTimeStamp}`);
+    console.log( `${blockHeader.number} ${blockHeader.hash} ${blockHeader.extraData} ${blockHeader.timestamp} ${new Date(thisTimeStamp * 1000).toUTCString()} ${lastTimeStamp - thisTimeStamp}`);
 
     lastTimeStamp = thisTimeStamp;
     

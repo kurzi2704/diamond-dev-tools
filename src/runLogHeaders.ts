322da6b9885e0911cd0d180bac18b68d5a7d2892
Thomas Haller <Thomas.Haller@lab10.coop>
2022-03-19 01:40:55 +0100

    log headers now logs in CSV format.

--- src/runLogHeaders.ts
+++ src/runLogHeaders.ts
@@ -2,6 +2,8 @@
 import { ConfigManager } from './configManager';
 import { ContractManager } from './contractManager'; 
 
+// const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
+
 async function logHeaders() {
 
   const web3 = ConfigManager.getWeb3();
@@ -16,19 +18,26 @@ async function logHeaders() {
 
   let lastTimeStamp = 0;
 
+  //console.log(``);
+  console.log( `"block";"hash";"extraData";"timestamp";"date";"duration"`);
+
+
+
+
   for(let i = numOfBlocksToDisplay; i >= 0; i--) {
     
     const blockToAnalyse = i;
     const blockHeader = await web3.eth.getBlock(blockToAnalyse);
 
     const thisTimeStamp = Number.parseInt(String(blockHeader.timestamp));
-    console.log( `${blockHeader.number} ${blockHeader.hash} ${blockHeader.extraData} ${blockHeader.timestamp} ${new Date(thisTimeStamp * 1000).toUTCString()} ${lastTimeStamp - thisTimeStamp}`);
+    console.log( `${blockHeader.number};"${blockHeader.hash}";"${blockHeader.extraData}";${blockHeader.timestamp};"${new Date(thisTimeStamp * 1000).toISOString()}";${lastTimeStamp - thisTimeStamp}`);
+    //console.log( `${blockHeader.number} ${blockHeader.hash} ${blockHeader.extraData} ${blockHeader.timestamp} ${new Date(thisTimeStamp * 1000).toUTCString()} ${lastTimeStamp - thisTimeStamp}`);
 
     lastTimeStamp = thisTimeStamp;
     
     //if (blockHeader.timestamp is number)
     //lastTimeStamp = Number.parseInt(blockHeader.timestamp);
-
+    
 
   }
 }

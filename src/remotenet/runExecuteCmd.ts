6f5db4bd3c6f3333975a6fba2c13e23c65dea6b4
Thomas Haller <Thomas.Haller@lab10.coop>
2022-04-18 23:37:31 +0200

    execute cmd script

---
+++ src/remotenet/runExecuteCmd.ts
@@ -0,0 +1,10 @@
+import { executeOnRemotes } from './executeOnRemotes';
+import { getNodesFromCliArgs, } from './remotenetArgs';
+
+async function run() {
+  
+  const nodesToExecute = await getNodesFromCliArgs();
+  executeOnRemotes(`screen -wipe`, nodesToExecute);
+}
+
+run();

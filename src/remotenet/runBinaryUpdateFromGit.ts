1dda8d5e458bb6b1a27ccb5af9568c2860b6d418
Thomas Haller <Thomas.Haller@lab10.coop>
2022-04-21 10:47:39 +0200

    script for binary update from git.

---
+++ src/remotenet/runBinaryUpdateFromGit.ts
@@ -0,0 +1,31 @@
+import { cmdR } from "../remoteCommand";
+import { getNodesFromCliArgs } from "./remotenetArgs";
+
+async function run() {
+  const nodes = await getNodesFromCliArgs();
+
+  nodes.forEach(n=> {
+    const nodeName = `hbbft${n.nodeID}`;
+    console.log(`=== ${nodeName} ===`);
+
+    console.log(`stopping node ${nodeName}`);
+    try {
+      cmdR(nodeName, `screen -X -S node_test quit`);
+    } catch(e) {
+      console.log('ignored error.');
+    }
+    
+    console.log(`pulling repo ${nodeName}`);
+    cmdR(nodeName, `cd ~/dmdv4-testnet && git pull`);
+
+    console.log(`building ${nodeName}`);
+    cmdR(nodeName, `cd ~/dmdv4-testnet && ~/dmdv4-testnet/build-from-source.sh`);
+
+    console.log(`starting ${nodeName}`);
+    cmdR(nodeName, `cd dmdv4-testnet && screen -S node_test -d -m ~/dmdv4-testnet/start.sh`);
+  });
+}
+
+
+//todo find better command, this kind of hard kills it.
+run();

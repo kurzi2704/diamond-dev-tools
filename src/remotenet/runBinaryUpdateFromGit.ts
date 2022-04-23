65c771f1d2955e07c453dbf6431b5924fc97bde4
Thomas Haller <Thomas.Haller@lab10.coop>
2022-04-23 11:52:31 +0200

    binary update from git: start node even after warnings. OR start old diamond-node version in case of error.

--- src/remotenet/runBinaryUpdateFromGit.ts
+++ src/remotenet/runBinaryUpdateFromGit.ts
@@ -18,9 +18,15 @@ async function run() {
     console.log(`pulling repo ${nodeName}`);
     cmdR(nodeName, `cd ~/dmdv4-testnet && git pull`);
 
-    console.log(`building ${nodeName}`);
-    cmdR(nodeName, `cd ~/dmdv4-testnet && ~/dmdv4-testnet/build-from-source.sh`);
+    try {
+      console.log(`building ${nodeName}`);
+      cmdR(nodeName, `cd ~/dmdv4-testnet && ~/dmdv4-testnet/build-from-source.sh`);
 
+    } catch (e) {
+      // compile results in non-zero exit code if there are warnings, so we ignore them.
+    }
+
+    
     console.log(`starting ${nodeName}`);
     cmdR(nodeName, `cd dmdv4-testnet && screen -S node_test -d -m ~/dmdv4-testnet/start.sh`);
   });

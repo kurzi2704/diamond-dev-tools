511736a9918c9c946bb22a6d20d5b6d5c1f48ecb
Thomas Haller <Thomas.Haller@lab10.coop>
2022-06-16 22:03:51 +0200

    start remotenet now checks for existing screen session to avoid multiple sessions.

--- src/remotenet/runStartRemotenet.ts
+++ src/remotenet/runStartRemotenet.ts
@@ -1,4 +1,5 @@
 import * as child from 'child_process';
+import { cmdR } from '../remoteCommand';
 import { executeOnRemotes } from './executeOnRemotes';
 import { getNodesFromCliArgs, parseRemotenetArgs } from './remotenetArgs';
 
@@ -8,6 +9,27 @@ async function run() {
   console.log('operating in: ' + pwdResult.toString());
 
   const nodesToExecute = await getNodesFromCliArgs();
+
+  for (const n of nodesToExecute) {
+
+    const nodeName = `hbbft${n.nodeID}`;
+    try {
+
+      console.log(`=== ${nodeName} ===`);
+
+      const runningScreens = cmdR(nodeName, `screen -ls`);
+
+      if (runningScreens.includes('node_test')) {
+        console.log('WARNING: node_test screen already running!!');
+      } else {
+        cmdR(nodeName, `cd dmdv4-testnet && screen -S node_test -d -m ~/dmdv4-testnet/start.sh`);
+      }
+
+      
+    } catch (e) {
+      console.log(`ignoring error on ${nodeName}`);
+    }
+  }
 
   executeOnRemotes(`cd dmdv4-testnet && screen -S node_test -d -m ~/dmdv4-testnet/start.sh`, nodesToExecute);
 

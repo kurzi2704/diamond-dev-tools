6cba3fea66336725804dfc2bb363517c819bb00b
Thomas Haller <Thomas.Haller@lab10.coop>
2022-09-15 18:04:33 +0200

    script for searchPerformancePlateau

--- src/tests/runSearchPerformancePlateau.ts
+++ src/tests/runSearchPerformancePlateau.ts
@@ -4,6 +4,7 @@ import { ContractManager } from "../contractManager";
 import { sleep } from "../utils/time";
 import fs from "fs";
 import { awaitTransactions } from "../tools/awaitTransactions";
+import { SignedTransaction } from "web3-core";
 
 
 function toNumber(value: string | number) : number {
@@ -27,7 +28,7 @@ async function run() {
 
   const wallets = ConfigManager.insertWallets(web3, 50);
 
-  const defaultGasPrice = '1000000000000';
+  const defaultGasPrice = '1000000000';
   console.log("Warmup: Funding Accounts.");
 
   let confirmed = 0;
@@ -52,34 +53,58 @@ async function run() {
   console.log('Ramping up - finding plateu');
 
   let txPerAccount = 1;
-  let outputFile = 'find-plateau.csv';
+  let outputFile = 'jupyter/data/hbbft/plateau/find-plateau.csv';
   console.log(`writing output to ${outputFile}`);
-  fs.writeFileSync(outputFile, "tx-per-account;num-of-accounts;total-txs;number-of-blocks;sum-of-block-time;blockStart;blockEnd;block-per-second;txs-per-second;\n");
-  
-
+  fs.writeFileSync(outputFile, `"tx_per_account","num_of_accounts","total_txs","number_of_blocks","sum_of_block_time","blockStart","blockEnd","block_per_second","txs_per_second"\n`);
+   
+  // console.log(`sending one transaction to warmup block production`);
   // make a transaction to ensure the start of block production on hbbft.
-  web3.eth.sendTransaction({ from: web3.eth.defaultAccount!, to: web3.eth.defaultAccount!, nonce: nonceFeed, value: web3.utils.toWei('1', "ether"), gas: "21000", gasPrice: defaultGasPrice});
+  //  web3.eth.sendTransaction({ from: web3.eth.defaultAccount!, to: web3.eth.defaultAccount!, nonce: nonceFeed, value: web3.utils.toWei('1', "ether"), gas: "21000", gasPrice: defaultGasPrice});
 
-  while (txPerAccount < 10) { // this while condition is kind of a max - we early exit if we have found a plateau.
+  while (txPerAccount < 50) { // this while condition is kind of a max - we early exit if we have found a plateau.
 
+    
     const blockStart = await web3.eth.getBlockNumber();
     const blockStartInstance = await web3.eth.getBlock(blockStart);
     const blockStartTime = toNumber(blockStartInstance.timestamp);
 
     let totalTxs = txPerAccount * wallets.length;
     let transactionHashesToConfirm : Array<string> = [];
+
+    console.log(`Starting test section with ${txPerAccount} transactions per account with ${wallets.length} accounts. Total TX: ${totalTxs}`);
+
     for(const wallet of wallets) {
 
       let nonce = await web3.eth.getTransactionCount(wallet.address);
-
+      // let signedTransaction : Array<SignedTransaction> = [];
       for(let i = 0; i<txPerAccount; i++) {
-        web3.eth.sendTransaction({ from: wallet.address, to: wallet.address, value: '0', nonce, gas: "21000", gasPrice: defaultGasPrice})
-        .once("transactionHash", (transactionHash) => {
-          transactionHashesToConfirm.push(transactionHash);
-        });
+        const txConfig = {from: wallet.address, to: wallet.address, value: '0', nonce, gas: "21000", gasPrice: defaultGasPrice};
+        const signed =  await wallet.signTransaction(txConfig);
+        
+        
+        if (signed.transactionHash) {
+          transactionHashesToConfirm.push(signed.transactionHash);
+        }
+        else {
+          console.log('no hash for tx', txConfig);
+        }
+
+        web3.eth.sendSignedTransaction(signed.rawTransaction!);
+        //web3.eth.signTransaction({});
+        // web3.eth.sendTransaction({ from: wallet.address, to: wallet.address, value: '0', nonce, gas: "21000", gasPrice: defaultGasPrice})
+        // .once("transactionHash", (transactionHash) => {
+        //   transactionHashesToConfirm.push(transactionHash);
+        // });
         nonce++;
       }
     }
+
+    // console.log(`waiting until all ${totalTxs} transactions was sent`);
+
+    // while(transactionHashesToConfirm.length < totalTxs) {
+    //   console.log(`${transactionHashesToConfirm.length}/${totalTxs} ( ${(transactionHashesToConfirm.length/totalTxs).toPrecision(4)}%)`);
+    //   sleep(1000);
+    // }
 
     console.log(`transactions Sent: ${transactionHashesToConfirm.length} scanning blocks to verify transaction receipts...`);
     
@@ -95,7 +120,7 @@ async function run() {
     const blocksPerSecond = numOfBlocks / sumOfBlockTime;
     const txsPerSecond = totalTxs / sumOfBlockTime;
 
-    fs.appendFileSync(outputFile, `${txPerAccount};${wallets.length};${totalTxs};${numOfBlocks};${sumOfBlockTime};${blockStart};${blockEnd};${blocksPerSecond.toPrecision(4)};${txsPerSecond.toPrecision(4)};\n`);
+    fs.appendFileSync(outputFile, `"${txPerAccount}","${wallets.length}","${totalTxs}","${numOfBlocks}","${sumOfBlockTime}","${blockStart}","${blockEnd}","${blocksPerSecond.toPrecision(4)}","${txsPerSecond.toPrecision(4)}"\n`);
     txPerAccount++;
   }
   

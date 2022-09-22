2f4dfa07d1da072f2155591526d2a4db9b3f74a6
Thomas Haller <Thomas.Haller@lab10.coop>
2022-09-22 22:20:20 +0200

    build from source prebuild now supports profiles

---
+++ src/remotenet/buildFromSource.ts
@@ -0,0 +1,8 @@
+import { ConfigManager } from "../configManager";
+
+
+export function getBuildFromSourceCmd() {
+  const config = ConfigManager.getConfig();
+  const dir = `~/${config.installDir}/openethereum-3.x/`;
+  return `cd ${dir} && git pull && cargo build --profile=${config.openEthereumProfile} --all -j 1 && cp target/${config.openEthereumProfile}/openethereum ../openethereum`;
+}

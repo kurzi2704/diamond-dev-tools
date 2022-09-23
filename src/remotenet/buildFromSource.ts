78438844ff3733410fc64384043f76d82fafa3a4
Thomas Haller <Thomas.Haller@lab10.coop>
2022-09-23 18:54:29 +0200

    hardcoded  cargo location

--- src/remotenet/buildFromSource.ts
+++ src/remotenet/buildFromSource.ts
@@ -4,5 +4,5 @@ import { ConfigManager } from "../configManager";
 export function getBuildFromSourceCmd() {
   const config = ConfigManager.getConfig();
   const dir = `~/${config.installDir}/openethereum-3.x/`;
-  return `cd ${dir} && git pull && cargo build --profile=${config.openEthereumProfile} --all -j 1 && cp target/${config.openEthereumProfile}/openethereum ../openethereum`;
+  return `cd ${dir} && git pull && ~/.cargo/bin/cargo build --profile=${config.openEthereumProfile} --all -j 1 && cp target/${config.openEthereumProfile}/openethereum ../openethereum`;
 }

9b1aca0dcfac55a51b47ecbae48a782995eb3077
Thomas Haller <thomashaller@gmx.at>
2022-10-07 14:58:06 +0200

    included RUSTFLAGS='-C target-cpu=native' for compiling from source

--- src/remotenet/buildFromSource.ts
+++ src/remotenet/buildFromSource.ts
@@ -4,5 +4,5 @@ import { ConfigManager } from "../configManager";
 export function getBuildFromSourceCmd() {
   const config = ConfigManager.getConfig();
   const dir = `~/${config.installDir}/openethereum-3.x/`;
-  return `cd ${dir} && git pull && ~/.cargo/bin/cargo build --profile=${config.openEthereumProfile} --all -j 1 && cp target/${config.openEthereumProfile}/openethereum ../openethereum`;
+  return `cd ${dir} && git pull && export RUSTFLAGS='-C target-cpu=native' &&  ~/.cargo/bin/cargo build --profile=${config.openEthereumProfile} --all -j 1 && cp target/${config.openEthereumProfile}/openethereum ../openethereum && echo openethereum binary was copied`;
 }

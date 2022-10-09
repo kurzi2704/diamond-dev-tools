96d0db4b9fc6d5d1fff97de33b7d1ca03e1a1a0a
Thomas Haller <thomashaller@gmx.at>
2022-10-09 18:11:12 +0200

    log headers (npm run net-csv-eaders) now logs "num_of_validators","transaction_count","txs_per_sec" in addation, and uses python numby compatible csv format.

--- src/runLogHeaders.ts
+++ src/runLogHeaders.ts
@@ -19,18 +19,22 @@ async function logHeaders() {
   let lastTimeStamp = 0;
 
   //console.log(``);
-  console.log( `"block";"hash";"extraData";"timestamp";"date";"duration"`);
+  console.log( `"block","hash","extraData","timestamp","date","duration","num_of_validators","transaction_count","txs_per_sec"`);
 
 
 
+  const contractManager = ContractManager.get();
 
   for(let i = numOfBlocksToDisplay; i >= 0; i--) {
     
     const blockToAnalyse = i;
     const blockHeader = await web3.eth.getBlock(blockToAnalyse);
-
     const thisTimeStamp = Number.parseInt(String(blockHeader.timestamp));
-    console.log( `${blockHeader.number};"${blockHeader.hash}";"${blockHeader.extraData}";${blockHeader.timestamp};"${new Date(thisTimeStamp * 1000).toISOString()}";${lastTimeStamp - thisTimeStamp}`);
+    const duration =  - (thisTimeStamp - lastTimeStamp);
+    const transaction_count = blockHeader.transactions.length;
+    const num_of_validators = await (await contractManager.getValidators(blockToAnalyse)).length;
+    const txs_per_sec = transaction_count / duration;
+    console.log( `"${blockHeader.number}","${blockHeader.hash}","${blockHeader.extraData}","${blockHeader.timestamp}","${new Date(thisTimeStamp * 1000).toISOString()}","${duration}","${num_of_validators}","${transaction_count}","${txs_per_sec.toFixed(4)}"`);
     //console.log( `${blockHeader.number} ${blockHeader.hash} ${blockHeader.extraData} ${blockHeader.timestamp} ${new Date(thisTimeStamp * 1000).toUTCString()} ${lastTimeStamp - thisTimeStamp}`);
 
     lastTimeStamp = thisTimeStamp;

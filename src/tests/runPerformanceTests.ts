260d09319abd070f94ac45a052e5155d7ab4e96d
Thomas Haller <thomashaller@gmx.at>
2022-10-16 21:13:45 +0200

    npm run test-performance now uses fast Transaction sender for sending the funding transactions as well. This results in less often occuring RPC Errors.

--- src/tests/runPerformanceTests.ts
+++ src/tests/runPerformanceTests.ts
@@ -10,99 +10,49 @@ import { sleep } from "../utils/time";
 async function runPerformanceTests() {
 
   const web3 = ConfigManager.getWeb3();
-
   const { toWei, toBN } = web3.utils;
-
   const sendAccounts : Array<Account> = [];
 
-  
   //const minBalance = toBN(toWei('1', 'ether'));
-
-  
   // min gas price delivers wrong information from rpc.
 
   const minGasPrice = '1000000000';
-
   const minBalance = toBN(minGasPrice).mul(toBN(21000));
-
   console.log("Min gase Price:", minGasPrice);
 
 
   // const fundingPromises : Array<PromiEvent<TransactionReceipt>> = [];
 
   let transactionSuccesses = 0;
-  let nonce = await web3.eth.getTransactionCount(web3.eth.defaultAccount!);
-
+  // let nonce = await web3.eth.getTransactionCount(web3.eth.defaultAccount!);
   
   let fastTxSender = new FastTxSender(web3);
-
   console.log('Creating accounts for wallet, using funding address: ', web3.eth.defaultAccount);
-  for(let i = 1; i <= 10; i++) {
+  for(let i = 1; i <= 10000; i++) {
 
     
     const account = web3.eth.accounts.create(`test${i}` );
     web3.eth.accounts.wallet.add(account);
     sendAccounts.push(account);
-    
     const balance = web3.utils.toBN(await web3.eth.getBalance(account.address));
-
 
     if (balance.lt(minBalance)) {
       console.log(`Funding: `, account.address);
-      // fundingPromises.push(
-      //   web3.eth.sendTransaction({ from: web3.eth.defaultAccount!, to: account.address, value: minBalance, gas: 21000, gasPrice: minGasPrice })
-      // );
-
-      for (let j = 1; j < 100; j++) {
-        const calcedGasPrice = toBN(minGasPrice).mul(toBN(j)).toString();
-        try {
-          const tx = { from: web3.eth.defaultAccount!, to: account.address, value: minBalance, gas: 21000, gasPrice: calcedGasPrice, nonce: nonce };
-          // console.log("sending transaction: ", tx);
-          web3.eth.sendTransaction(tx).once("confirmation",(confirmationNumber: number, receipt: TransactionReceipt, latestBlockHash?: string | undefined) => {
-            transactionSuccesses++;
-          })
-          break; // we are done with this node.
-        } catch (e) {
-          
-          if (isErrorWithMessage(e)) {
-            const error = toErrorWithMessage(e);
-            if (error.message.includes("Transaction gas price supplied is too low.")) {
-              console.log("Detected Gas Price to low - retrying.");
-              continue;
-            } else {
-              console.log("Error during send transaction.");
-              throw e;
-            }
-
-          } else {
-            console.log("Unexpected Error.", e);
-            throw e;
-          }
-        }
-      }
+      const tx = { from: web3.eth.defaultAccount!, to: account.address, value: minBalance, gas: 21000, gasPrice: minGasPrice };
+      fastTxSender.addTransaction(tx);
     }
 
-    nonce ++;
+    // nonce ++;
   }
 
-  //awaiting funding promises:
+  console.log('sending Fund accounts transactions...');
+  await fastTxSender.sendTxs();
 
-  console.log('awaiting funding promises.');
-
-  while(transactionSuccesses < sendAccounts.length) {
-    console.log(`confirmed: ${transactionSuccesses} / ${sendAccounts.length}`);
-    await sleep(1000);
-  }
-
-  console.log("All funding transactions confirmed.");
-
-
-  console.log('all accounts funded');
-
-  let confirmedTxs = 0;
-
+  console.log('waiting for transactions to be mined...');
+  await fastTxSender.awaitTxs();
   
-
+  console.log("All funding transactions confirmed.");
+  
   for (const account of sendAccounts) {
     await fastTxSender.addTransaction({ from: account.address, to: account.address, value: 0, gas: 21000, gasPrice: minGasPrice });
   }

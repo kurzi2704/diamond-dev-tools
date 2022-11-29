4dc3f64bd03d017f17ca21c087dce53ad5d449cf
Thomas Haller <thomashaller@gmx.at>
2022-11-29 23:18:06 +0100

    localnet-create-mnemonic

---
+++ src/localnet/create-random-mnemonic.ts
@@ -0,0 +1,28 @@
+
+//import bip39 from "bip39";
+import fs from "fs";
+//import Web3 from "web3";
+import { generateAddressesFromSeed } from "../utils";
+// creates a random mnemonic and writes the first address out to .mainaddress
+
+
+
+async function run() {
+  
+  if (fs.existsSync(".mnemonic")) {
+    console.error(".mnemonic file already exists");
+    process.exit(1);
+  }
+
+  let bip39 = require("bip39");
+  
+  
+  const mnemonic = bip39.generateMnemonic()
+  let mainAddress = generateAddressesFromSeed(mnemonic, 1)[0].address;
+  
+  fs.writeFileSync(".mnemonic", mnemonic);
+  fs.writeFileSync(".mainaddress", mainAddress);
+
+}
+
+run();

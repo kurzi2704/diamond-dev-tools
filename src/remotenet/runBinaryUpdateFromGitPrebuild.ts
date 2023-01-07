a3b382a6d25204a2f3f897c4f5e5ec139322f0dd
Thomas Haller <Thomas.Haller@lab10.coop>
2023-01-07 17:32:40 +0100

    improved async build script

--- src/remotenet/runBinaryUpdateFromGitPrebuild.ts
+++ src/remotenet/runBinaryUpdateFromGitPrebuild.ts
@@ -4,21 +4,60 @@ import { getBuildFromSourceCmd } from './buildFromSource';
 import { getNodesFromCliArgs } from './remotenetArgs';
 
 async function run() {
+
   const nodes = await getNodesFromCliArgs();
 
-  const config = ConfigManager.getConfig();
-  const buildCmd = getBuildFromSourceCmd();
-  // nohup sh -c 'wget "$0" && wget "$1"' "$url1" "$url2" > /dev/null &
+  const gitClonePromises : Promise<string>[] = [];
 
-  const promises = nodes.map((n) => cmdRemoteAsync(n.sshNodeName(), `cd ~/${config.installDir} && git pull && nohup sh -c '${buildCmd}'`));
+  for (const n of nodes) {
+    const nodeName = `hbbft${n.nodeID}`;
+    console.log(`=== ${nodeName} ===`);
 
-  console.log('awaiting promises.');
-  for (const p in promises) {
-    await p;
-    console.log('ok');
+    console.log(`stopping node ${nodeName}`);
+    try {
+      cmdR(nodeName, 'screen -X -S node_test quit');
+    } catch (e) {
+      console.log('ignored error.');
+    }
+
+    const config = ConfigManager.getConfig();
+
+    console.log(`pulling repo ${nodeName}`);
+    gitClonePromises.push(cmdRemoteAsync(nodeName, `cd ~/${config.installDir} && git checkout start.sh && git pull`));
+
   }
 
-  console.log('all work done - build might take 1 hour');
+  console.log('All repositories are being pulled. This may take a while.');
+  await Promise.all(gitClonePromises);
+  console.log('All repositories have been pulled.');
+
+  const buildPromises : Promise<string>[] = [];
+
+  for (const n of nodes) {
+    const buildCmd = getBuildFromSourceCmd();
+    // cmdR(nodeName, buildCmd);
+    buildPromises.push(cmdRemoteAsync(n.sshNodeName(), getBuildFromSourceCmd()));
+  }
+
+
+  console.log('waiting for build');
+
+  
+  for (let i of buildPromises.keys()) { 
+    
+    const promise = buildPromises[i];
+    const node = nodes[i];
+
+    let result = await promise;
+
+    console.log(`=== ${node.sshNodeName()} ===`);
+    console.log(result);
+    console.log(`= END = ${node.sshNodeName()} = END =`);
+  }
+
+
+  console.log('All nodes have been build and updated.');
+
 }
 
 // todo find better command, this kind of hard kills it.

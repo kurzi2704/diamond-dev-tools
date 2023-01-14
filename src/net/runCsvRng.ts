4984ed08c5921ce7b778c7dce488cc4074f9c326
Thomas Haller <Thomas.Haller@lab10.coop>
2023-01-14 14:34:17 +0100

    net-csv-rng now uses new historic functionality and checks the results.

--- src/net/runCsvRng.ts
+++ src/net/runCsvRng.ts
@@ -1,5 +1,6 @@
 import { ConfigManager } from "../configManager";
 import { ContractManager } from "../contractManager";
+import { blockTimeAsUTC } from "../utils/dateUtils";
 
 
 
@@ -7,25 +8,46 @@ async function run() {
 
   const web3 = ConfigManager.getWeb3();
 
+  const toBN =  web3.utils.toBN;
+
   const contractManager = ContractManager.get();
 
   const rng_contract = await ContractManager.get().getRandomHbbft();
 
   const latestBlockNumber = await web3.eth.getBlockNumber();
-  let numberOfBlocksToLog = 1000;
+  //let numberOfBlocksToLog = 1000;
 
-  if ( numberOfBlocksToLog > latestBlockNumber ){
-    numberOfBlocksToLog = latestBlockNumber;
-  }
+  // if ( numberOfBlocksToLog > latestBlockNumber ){
+  //   numberOfBlocksToLog = latestBlockNumber;
+  // }
 
 
-  for(let blockNumber = latestBlockNumber - numberOfBlocksToLog; blockNumber <= latestBlockNumber; blockNumber++) {
+  for(let blockNumber = latestBlockNumber; blockNumber > 1; blockNumber--) {
     const block = await web3.eth.getBlock(blockNumber);
     
-    let extraData = block.extraData;
-    let rng_from_contract = await rng_contract.methods.currentSeed().call({}, blockNumber);
+    const extraData = block.extraData;
+    const timestamp = blockTimeAsUTC(block.timestamp);
+    let rng_from_contract = toBN(0);
+    let rng_from_contract_historic = toBN(0);
 
-    console.log(`block: ${blockNumber} extraData: ${extraData} rng_from_contract: ${rng_from_contract}`);
+    try {
+      rng_from_contract = web3.utils.toBN(await rng_contract.methods.currentSeed().call({}, blockNumber));
+    } catch (e){
+
+    }
+
+    try {
+      rng_from_contract_historic  = web3.utils.toBN(await rng_contract.methods.getSeedHistoric(blockNumber).call());
+    } catch (e) {
+      
+    }
+
+    const extraDataBN = web3.utils.toBN(extraData);
+
+    const extraDataEqualsSeed = extraDataBN.eq(rng_from_contract);
+    const extraDataEqualsHistoric = extraDataBN.eq(rng_from_contract_historic);
+
+    console.log(`block: ${blockNumber} time: ${timestamp} extraDataEqualsSeed: ${extraDataEqualsSeed} extraDataEqualsHistoric: ${extraDataEqualsHistoric} extraData: ${extraData} rng_from_contract: ${rng_from_contract.toString('hex')} rng_from_contract_historic: ${rng_from_contract_historic.toString('hex')}`);
     // block.timestamp
     
   }

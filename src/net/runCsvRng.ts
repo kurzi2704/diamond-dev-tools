7b76b99c9c4cdee7a3f5cad57d3a33ba3cedadd7
Thomas Haller <thomashaller@gmx.at>
2023-02-05 22:01:26 +0100

    csv-rng now also reports the health status

--- src/net/runCsvRng.ts
+++ src/net/runCsvRng.ts
@@ -12,7 +12,7 @@ async function run() {
 
   const contractManager = ContractManager.get();
 
-  const rng_contract = await ContractManager.get().getRandomHbbft();
+  const rng_contract = await contractManager.getRandomHbbft();
 
   const latestBlockNumber = await web3.eth.getBlockNumber();
   //let numberOfBlocksToLog = 1000;
@@ -39,15 +39,23 @@ async function run() {
     try {
       rng_from_contract_historic  = web3.utils.toBN(await rng_contract.methods.getSeedHistoric(blockNumber).call());
     } catch (e) {
-      
+      console.log("Get historic seed failed:", e);
     }
 
+    let health_from_contract = false;
+    try {
+      let health_from_contract_BN = web3.utils.toBN(await rng_contract.methods.currentSeed().call({}, blockNumber));
+      health_from_contract = !health_from_contract_BN.isZero();
+    } catch (e) {
+      console.log("Get health faled:", e);
+    }
+ 
     const extraDataBN = web3.utils.toBN(extraData);
 
     const extraDataEqualsSeed = extraDataBN.eq(rng_from_contract);
     const extraDataEqualsHistoric = extraDataBN.eq(rng_from_contract_historic);
 
-    console.log(`block: ${blockNumber} time: ${timestamp} extraDataEqualsSeed: ${extraDataEqualsSeed} extraDataEqualsHistoric: ${extraDataEqualsHistoric} extraData: ${extraData} rng_from_contract: ${rng_from_contract.toString('hex')} rng_from_contract_historic: ${rng_from_contract_historic.toString('hex')}`);
+    console.log(`block: ${blockNumber} time: ${timestamp} extraDataEqualsSeed: ${extraDataEqualsSeed} extraDataEqualsHistoric: ${extraDataEqualsHistoric} health: ${health_from_contract} extraData: ${extraData} rng_from_contract: ${rng_from_contract.toString('hex')} rng_from_contract_historic: ${rng_from_contract_historic.toString('hex')}`);
     // block.timestamp
     
   }

aa27af333370cbc7702de68905d68a76c90973e9
Thomas Haller <thomashaller@gmx.at>
2023-06-09 15:57:52 +0000

    fixed print net state

--- src/net/runPrintNetState.ts
+++ src/net/runPrintNetState.ts
@@ -1,6 +1,6 @@
 import { ContractManager } from '../contractManager';
 import { NodeManager } from './nodeManager';
-import { printState } from './printKeyGenState';
+import { printState } from './printState';
 
 const nodeManager = NodeManager.get();
 const contractManager = ContractManager.get();

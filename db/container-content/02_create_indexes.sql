665243515fa32faa8c5782ba306596e12b475761
Thomas Haller <thomashaller@gmx.at>
2023-06-19 08:09:44 +0000

    removed double declaration of index

--- db/container-content/02_create_indexes.sql
+++ db/container-content/02_create_indexes.sql
@@ -2,4 +2,3 @@
 
 CREATE UNIQUE INDEX ix_headers_block_time ON headers(block_time) INCLUDE (block_number);
 CREATE UNIQUE INDEX ix_headers_block_number_with_time ON headers(block_number) INCLUDE (block_time);
-CREATE UNIQUE INDEX ix_headers_posdao_epoch ON headers(block_number) INCLUDE (block_time);

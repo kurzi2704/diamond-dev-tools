4b6c6f2c09d5423deac96742d02e9b37feb12f2a
Thomas Haller <thomashaller@gmx.at>
2023-09-07 19:19:42 +0200

    create prometheus config.

--- src/remotenet/runCreatePrometheusScrapeConfig.ts
+++ src/remotenet/runCreatePrometheusScrapeConfig.ts
@@ -1,6 +1,9 @@
 
 
 import yaml from 'js-yaml';
+import { getNodesFromCliArgs, parseRemotenetArgs } from './remotenetArgs';
+import { loadNodeInfosFromTestnetDirectory } from '../net/nodeInfo';
+import { getIP } from './remotenetUtils';
 
 
 
@@ -10,9 +13,10 @@ function getTarget(ip: string, port: number, instanceName: string) : any {
     target.targets = [`${ip}:${port}`];
 
     let labels : any = {};
-    labels.location = 'Munich';
-    labels.name = "hbbft1";
+    // labels.location = 'Munich';
+    labels.name = instanceName;
 
+    target.labels = labels;
     return target
 }
 
@@ -37,18 +41,38 @@ async function createPrometheusScrapeConfig() {
 
     let targets : any[] = [];
 
-    let target1 : any = {};
-    target1.targets = ['localhost:9100'];
+    const nodeInfos = loadNodeInfosFromTestnetDirectory();
 
-    let labels1 : any = {};
-    labels1.location = 'Munich';
-    labels1.name = "hbbft1";
+    // function getIP(nodeID: number) : string | undefined {
+
+    //     if (nodeInfos) {
+    //         return nodeInfos.ip_addresses[nodeID -1];
+    //     }
+
+    //     return undefined;
+    // }
+    
+    for (const node of await getNodesFromCliArgs()) {
+
+        let ip = getIP(node.nodeID);
+        
+        let target1 = getTarget(ip, 48700 + node.nodeID, `node${node.nodeID}`);
+        static_configs.push(target1);
+    
+    }
+
+    // let target1 : any = {};
+    // target1.targets = ['localhost:9100'];
+
+    // let labels1 : any = {};
+    // labels1.location = 'Munich';
+    // labels1.name = "hbbft1";
 
 
-    target1.labels = labels1;
-    //job.targets = targets;
+    // target1.labels = labels1;
+    // //job.targets = targets;
 
-    static_configs.push(target1);
+    
     scrape_configs.push(job);
 
     job.static_configs = static_configs;
@@ -62,3 +86,4 @@ async function createPrometheusScrapeConfig() {
 }
 
 createPrometheusScrapeConfig();
+

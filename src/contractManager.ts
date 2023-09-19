391e3064ec4ed2be946485f94df89f598bbf9125
Thomas Haller <thomashaller@gmx.at>
2023-09-19 16:44:53 +0200

    txPermission support for contract manager

--- src/contractManager.ts
+++ src/contractManager.ts
@@ -34,6 +34,9 @@ import {
   OrderedWithdrawalEvent,
   StakeChangedEvent
 } from './eventsVisitor';
+import { TxPermissionHbbft } from './abi/contracts';
+
+import JsonTxPermissionHbbft from './abi/json/TxPermissionHbbft.json';
 
 export enum KeyGenMode {
   NotAPendingValidator = 0,
@@ -45,7 +48,8 @@ export enum KeyGenMode {
 }
 
 export interface ContractAddresses {
-  validatorSetAddress: string
+  validatorSetAddress: string,
+  permissionContractAddress: string;
 }
 
 export type ContractEvent = AvailabilityEvent
@@ -81,6 +85,7 @@ export class ContractManager {
   private cachedStakingHbbft?: StakingHbbft;
   private cachedKeyGenHistory?: KeyGenHistory;
   private cachedRewardContract?: BlockRewardHbbftBase;
+  private cachedPermission?: TxPermissionHbbft;
 
   public constructor(public web3: Web3) { }
 
@@ -96,7 +101,7 @@ export class ContractManager {
   public static getContractAddresses(): ContractAddresses {
     //todo: query other addresses ?!
     // more intelligent contract manager that queries lazy ?
-    return { validatorSetAddress: '0x1000000000000000000000000000000000000001' }
+    return { validatorSetAddress: '0x1000000000000000000000000000000000000001', permissionContractAddress: `0x4000000000000000000000000000000000000001` }
   }
 
   public getValidatorSetHbbft(): ValidatorSetHbbft {
@@ -111,6 +116,19 @@ export class ContractManager {
     this.cachedValidatorSetHbbft = validatorSetContract;
 
     return validatorSetContract;
+  }
+
+
+  public getContractPermission() : TxPermissionHbbft {
+    if (this.cachedPermission) {
+      return this.cachedPermission;
+    }
+    const contractAddresses = ContractManager.getContractAddresses();
+    const abi: any = JsonTxPermissionHbbft.abi;
+    const permissionContract: any = new this.web3.eth.Contract(abi, contractAddresses.permissionContractAddress);
+    this.cachedPermission = permissionContract;
+    return permissionContract;
+
   }
 
   public getRegistry(): Registry {

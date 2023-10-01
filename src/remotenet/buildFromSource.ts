ea67ee057ae0f8e34a312dbb7be0958f6bb9b635
Thomas Haller <thomashaller@gmx.at>
2023-10-01 22:44:39 +0200

    deadlock detection now in place for build from source command.

--- src/remotenet/buildFromSource.ts
+++ src/remotenet/buildFromSource.ts
@@ -13,5 +13,5 @@ export function getBuildFromSourceCmd(checkout: boolean = false) {
   let checkoutCMD = checkout ? `&& git checkout ${config.openEthereumBranch} ` : '';
   
   let features = deadlockDetection ? ' --features deadlock_detection' : '';
-  return `cd ${dir} && git pull ${checkoutCMD} && export RUSTFLAGS='-C target-cpu=native' &&  ~/.cargo/bin/cargo build ${deadlockDetection} --profile=${config.openEthereumProfile} --all -j 1 && cp target/${config.openEthereumProfile}/diamond-node ../diamond-node`;
+  return `cd ${dir} && git pull ${checkoutCMD} && export RUSTFLAGS='-C target-cpu=native' &&  ~/.cargo/bin/cargo build ${features} --profile=${config.openEthereumProfile} --all -j 1 && cp target/${config.openEthereumProfile}/diamond-node ../diamond-node`;
 }

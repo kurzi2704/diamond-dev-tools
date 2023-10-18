df21d9bf6f44d513eef1e8d2555046e3caa447bd
Thomas Haller <thomashaller@gmx.at>
2023-10-18 13:58:49 +0200

    build from source script: cleanup to avoid version error.

--- src/remotenet/buildFromSource.ts
+++ src/remotenet/buildFromSource.ts
@@ -3,15 +3,17 @@ import { ConfigManager } from '../configManager';
 
 
 
-export function getBuildFromSourceCmd(checkout: boolean = false) {
+export function getBuildFromSourceCmd(checkout: boolean = false, copy: boolean = true) {
   const config = ConfigManager.getConfig();
   const installDir = ConfigManager.getNetworkConfig().installDir;
+  const diamondNodeBranch = ConfigManager.getOpenEthereumBranch();
 
   const deadlockDetection = ConfigManager.getOpenEthereumDeadlockDetection();
   const dir = `~/${installDir}/diamond-node-git/`;
 
-  let checkoutCMD = checkout ? `&& git checkout ${config.openEthereumBranch} ` : '';
+  let checkoutCMD = checkout ? `&& git checkout ${diamondNodeBranch} ` : '';
+  let copyCMD = copy ? `&& cp target/${config.openEthereumProfile}/diamond-node ../diamond-node` : '';
   
   let features = deadlockDetection ? ' --features deadlock_detection' : '';
-  return `cd ${dir} && git pull ${checkoutCMD} && export RUSTFLAGS='-C target-cpu=native' &&  ~/.cargo/bin/cargo build ${features} --profile=${config.openEthereumProfile} --all -j 1 && cp target/${config.openEthereumProfile}/diamond-node ../diamond-node`;
+  return `cd ${dir} && git pull ${checkoutCMD} && export RUSTFLAGS='-C target-cpu=native' &&  ~/.cargo/bin/cargo build ${features} --profile=${config.openEthereumProfile} --all -j 1 ${copyCMD}`;
 }

116ef8a364c7d7fcf9d1f08d78e22e40c90250a5
Thomas Haller <thomashaller@gmx.at>
2024-01-04 09:49:15 +0100

    runEarlyEpochEndTestNetwork set maxTriesForEpochSwitch to 5000;

--- src/localnet/tests/runEarlyEpochEndTestNetwork.ts
+++ src/localnet/tests/runEarlyEpochEndTestNetwork.ts
@@ -109,7 +109,7 @@ async function runEarlyEpochTestNetwork() {
     await createBlockAndRefresh();
     console.log('node 4 stopped, creating block should work, because of fault tolerance. but above early epoch end tolerance');
 
-    let maxTriesForEpochSwitch = 100;
+    let maxTriesForEpochSwitch = 5000;
 
     console.log('waiting for epoch switch to happen.');
 

f2fc55b6d3f06c26825421a16a7e57ad8975d947
Thomas Haller <thomashaller@gmx.at>
2024-01-06 18:00:15 +0100

    db: header import_complete coluimn

--- db/migrations/0006-header_import-complete.sql
+++ db/migrations/0006-header_import-complete.sql
@@ -1,7 +1,7 @@
 DO $$
     BEGIN
-        ALTER TABLE public.headers ADD COLUMN imort_finished boolean;
+        ALTER TABLE public.headers ADD COLUMN import_complete boolean;
     EXCEPTION
-        WHEN duplicate_column THEN RAISE NOTICE 'column imort_finished already exists in public.headers, skipping';
+        WHEN duplicate_column THEN RAISE NOTICE 'column import_complete already exists in public.headers, skipping';
     END
 $$;

574b51736b7a81f59960f658604a7a8fa18c36dd
Thomas Haller <thomashaller@gmx.at>
2024-09-08 22:28:14 +0200

    fast build: improved logging

--- src/remotenet/runBuildFromSourceFast.ts
+++ src/remotenet/runBuildFromSourceFast.ts
@@ -16,18 +16,32 @@ async function doRunBuildFromSource(n: NodeState): Promise<string> {
 
 async function runAllNodes() {
 
+
+
     let nodes = await getNodesFromCliArgs();
+    let finished = 0;
+    let error = 0;
     let results = nodes.map( (n) => {
          let result = doRunBuildFromSource(n);
          result.then(
             (result) => {
+                finished++;
                 console.log("finished: ", n.sshNodeName());
                 console.log(result);
                 console.log(" === end: ", n.sshNodeName(), " === ");
+                console.log("finished: ", finished);
+                console.log("error: ", error);
+                console.log("finalized: ", finished + error);
+                console.log("progress: ", (finished + error / nodes.length) * 100);
             }, (error) => {
+                error++;
                 console.log("Error: ", n.sshNodeName());
                 console.log(error);
                 console.log(" === end: ", n.sshNodeName(), " === ");
+                console.log("finished: ", finished);
+                console.log("error: ", error);
+                console.log("finalized: ", finished + error);
+                console.log("progress: ", (finished + error / nodes.length) * 100);
             });
          return result; 
     } );

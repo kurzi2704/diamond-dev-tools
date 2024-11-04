41413b323bbc9a7378c5f4740a4ffad8d8f88086
Thomas Haller <thomashaller@gmx.at>
2024-11-04 22:10:47 +0100

    tools: fund account now inserts wallets into web3, so we have access to all accounts

--- src/tools/runFundAccount.ts
+++ src/tools/runFundAccount.ts
@@ -5,8 +5,10 @@ async function run() {
 
   const web3 = ConfigManager.getWeb3();
   
+  ConfigManager.insertWallets(web3, 100);
+
   await web3.eth.sendTransaction({from: web3.eth.defaultAccount!, to: "0x74b711F61704925B20eb6Efc5e6ab0a1E5a063F2", value: web3.utils.toWei("1", "finney"), gas: 21000 });
-  //await web3.eth.sendTransaction({from: web3.eth.defaultAccount!, to: "0xC969dc0b07acE3e99d6C2636e26D80086a90b847", value: web3.utils.toWei("1", "finney"), gas: 21000 });
+  //await web3.eth.sendTransaction({from: "0xaF6719D1762D14e35a1dFb742B1d77D757391237", to: "0xC969dc0b07acE3e99d6C2636e26D80086a90b847", nonce: 1, value: web3.utils.toWei("1", "finney"), gas: 21000 });
 }
 
 run();

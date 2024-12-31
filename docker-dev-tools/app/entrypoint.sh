038d02f4ae9a0b789e6ac1bb38e79f947ee14b9f
Thomas Haller <thomashaller@gmx.at>
2024-12-31 18:46:26 +0100

    docker dev tools improvements

---
+++ docker-dev-tools/app/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
+    touch /$CONTAINER_FIRST_STARTUP
+    # place your script that you only want to run on first startup.
+    ./init.sh
+else
+    ./start.sh
+fi

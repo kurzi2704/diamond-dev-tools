322616a49c922b831e02b8d4d6a6e45b3e77dc7a
Thomas Haller <thomashaller@gmx.at>
2025-01-04 19:38:41 +0100

    docker-dev-tools: always start start.sh.

--- docker-dev-tools/app/entrypoint.sh
+++ docker-dev-tools/app/entrypoint.sh
@@ -3,13 +3,22 @@
 pwd
 ls -hal
 
+cd node_modules
+cd .bin
+
+ls -hal
+cd ..
+cd ..
+ls -hal
 # sleep 3600
+pwd
+
+sh
 
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     # place your script that you only want to run on first startup.
     ./init.sh
-else
-    ./start.sh
-fi
+
+./start.sh

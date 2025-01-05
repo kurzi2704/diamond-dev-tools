0ebdf7cbf29039c57f74d66374755b16901027ed
Thomas Haller <thomashaller@gmx.at>
2025-01-05 23:37:10 +0100

    docker-dev-tools: fixed entry point

--- docker-dev-tools/app/entrypoint.sh
+++ docker-dev-tools/app/entrypoint.sh
@@ -1,24 +1,9 @@
 #!/bin/sh
 
-pwd
-ls -hal
-
-cd node_modules
-cd .bin
-
-ls -hal
-cd ..
-cd ..
-ls -hal
-# sleep 3600
-pwd
-
-sh
-
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     # place your script that you only want to run on first startup.
     ./init.sh
-
+fi
 ./start.sh

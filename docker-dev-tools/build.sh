4a2a0e0dd3ec041b64a104a09661f566cdc2f35b
Thomas Haller <thomashaller@gmx.at>
2025-01-08 13:59:38 +0100

    docker files for Watchdog and State Server

--- docker-dev-tools/build.sh
+++ docker-dev-tools/build.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
 cd ..
-docker build --no-cache -t diamond-dev-tools -f docker-dev-tools/Dockerfile .
+
+CACHE="--no-cache"
+
+docker build $CACHE -t diamond-dev-tools-base -f docker-dev-tools/Dockerfile .
+docker build $CACHE -t diamond-watchdog -f docker-dev-tools/Dockerfile-Watchdog .
+docker build $CACHE -t diamond-state-server -f docker-dev-tools/Dockerfile-State-Server .

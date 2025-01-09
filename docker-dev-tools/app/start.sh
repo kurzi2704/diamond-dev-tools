fc9d8289ac7e1c8715907a32880acc3a048ff23a
Thomas Haller <thomashaller@gmx.at>
2025-01-09 15:31:43 +0100

    dont use bash within a docker container

--- docker-dev-tools/app/start.sh
+++ docker-dev-tools/app/start.sh
@@ -2,4 +2,4 @@
 
 echo "This container is designed to act as base container. This Container does nothing now, but stays open for debugging purposes. execute as docker run -ti diamond-dev-tools-base to debug into the container"
 
-bash
+sh

f18f8277623bfa63311f19fa371add4f251e65d3
Thomas Haller <thomashaller@gmx.at>
2025-01-14 17:22:18 +0100

    docker-dev-tools: DiamondDBSync

--- docker-dev-tools/build.sh
+++ docker-dev-tools/build.sh
@@ -2,8 +2,10 @@
 
 cd ..
 
-CACHE="--no-cache"
+#CACHE="--no-cache"
+CACHE=""
 
 docker build $CACHE -t diamond-dev-tools-base -f docker-dev-tools/Dockerfile .
 docker build $CACHE -t diamond-watchdog -f docker-dev-tools/Dockerfile-Watchdog .
 docker build $CACHE -t diamond-state-server -f docker-dev-tools/Dockerfile-State-Server .
+docker build $CACHE -t diamond-db-sync -f docker-dev-tools/Dockerfile-DB-Sync .
